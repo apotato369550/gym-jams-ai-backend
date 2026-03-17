95b47fd986e17eb0ca982dc134a3003ad982caad
John Andre Yap <johnandresyap510@gmail.com>
2026-03-17 13:54:24 +0800

    Gracefully degrade when can't connect to mysql database

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 import httpx
 
 from app.db.session import engine
-from app.db.models import Base# main.py
+from app.db.models import Base
 from app.routes.example import router as example_router
 
 load_dotenv()
@@ -14,14 +14,20 @@ app.include_router(example_router)
 
 # get api keys, urls ,and models
 OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
+print("API KEY:", os.getenv("OPENROUTER_API_KEY"))
 BASE_URL = os.getenv("OPENROUTER_BASE_URL")
 MODEL = os.getenv("MODEL")
 
 # connect to database
 @app.on_event("startup")
 async def startup():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+    try:
+        async with engine.begin() as conn:
+            await conn.run_sync(Base.metadata.create_all)
+        print("Database connected.")
+    except Exception as e:
+        print("Database unavailable. Running without Database")
+        print(e)
 
 # root 
 @app.get("/")
@@ -36,6 +42,7 @@ def health():
 # basic chat endpoint
 @app.post("/chat")
 async def chat(prompt: str):
+    print("API KEY:", os.getenv("OPENROUTER_API_KEY"))
     async with httpx.AsyncClient() as client:
         response = await client.post(
             f"{BASE_URL}/chat/completions",
